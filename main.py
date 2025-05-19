0bc8cb0fc68dc201e978ba8a6c7f2824057bf091
chiefe06 <efekaranhaci@gmail.com>
2025-05-19 19:00:26 +0300

    Updated comments

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ data_file = "user_data.xml"
 tag_file = "tag_data.pkl"
 
 def get_work_dir_from_xml():
-    """Reads the work directory from user_data.xml."""
+    #Reads the work directory from user_data.xml.
     tree = ET.parse(data_file)
     root = tree.getroot()
     work_dir = root.find("work_dir").text
     return work_dir
 
 def create_user_data_xml(work_dir):
-    """Creates the XML file with the work directory."""
+    #Creates the XML file with the work directory
     root = ET.Element("user_data")
     ET.SubElement(root, "work_dir").text = work_dir
     tree = ET.ElementTree(root)
     tree.write(data_file)
 
 def get_or_create_work_dir():
-    """Main function to either get from XML or prompt and save."""
+    #Main function to either get from XML or prompt and save
     if os.path.exists(data_file):
         try:
             work_dir = get_work_dir_from_xml()
@@ -43,15 +43,18 @@ def get_or_create_work_dir():
             print("Invalid directory. Try again.")
 
 def dump_folder_tag(folder_obj,save_file):
+    # Creates the pkl file with given FolderTag class object.
     with open(save_file, "wb") as f:
         pickle.dump(folder_obj, f)
 
 def read_pickle(save_file):
+    # Reads the tag data from tag_data.pkl.
     with open(save_file, "rb") as f:
         loaded_obj = pickle.load(f)
         return loaded_obj
 
 def get_or_create_folder_tag():
+    # Initial method to either read or store FolderTag object
     if os.path.exists(tag_file):
         try:
             root_folder = read_pickle(tag_file)
